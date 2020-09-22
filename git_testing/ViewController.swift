21fee7acdd80b3dfc7cf074f8b43e2c0977ad20d
qudanqi <qudanqi@msu.edu>
2020-09-21 22:33:16 -0400

    Initial Commit

---
+++ git_testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_testing
+//
+//  Created by dqu on 9/21/20.
+//  Copyright © 2020 dqu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ git_testingTests/git_testingTests.swift
@@ -0,0 +1,34 @@
+//
+//  git_testingTests.swift
+//  git_testingTests
+//
+//  Created by dqu on 9/21/20.
+//  Copyright © 2020 dqu. All rights reserved.
+//
+
+import XCTest
+@testable import git_testing
+
+class git_testingTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
